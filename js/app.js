ec47bf79d691efba727f1f10b264a3b4b2f37e85
Miranda Elliott <melliott@v4development.com>
2018-02-05 00:43:01 -0500

    Implement reset game, show modal when game won

--- js/app.js
+++ js/app.js
@@ -1,92 +1,130 @@
-/*
- * Create a list that holds all of your cards
- */
+let openCards = [], moveCount = 0, matchCount = 0, starRating = 3, startTime = performance.now();
+const deck = document.querySelector(".deck");
+const moves = document.querySelector(".moves");
+const stars = document.querySelector(".stars");
+const winModal = document.querySelector(".win-modal");
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+// Shuffle cards and reset card class
+function shuffleDeck() {
+  for (let i = deck.children.length; i >= 0; i--) {
+    const newCard = deck.children[Math.random() * i | 0];
+    newCard.classList = ["card"];
+    deck.appendChild(newCard);
+  }
 }
 
+// Restart game
+function restartGame() {
+  // Shuffle the deck
+  shuffleDeck();
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+  // Reset the star rating
+  starRating = 3;
+  for (let i=0; i<3; i++) {
+    resetStar(stars.children[i].querySelector("i"));
+  }
+
+  // Reset the move counter
+  moveCount = 0;
+  moves.textContent = moveCount;
+
+  // Reset the match counter
+  matchCount = 0;
+
+  // Reset the timer
+  startTime = performance.now();
+}
+
+// Set restart button event listener
+document.querySelector(".restart").addEventListener("click", function(e) {
+  restartGame();
+});
+
+// Set play again button event listener
+document.querySelector(".play-again").addEventListener("click", function(e) {
+  restartGame();
+  winModal.close();
+});
+
+// Toggle display on card
 function toggleCard(card) {
   card.classList.toggle("open");
   card.classList.toggle("show");
 }
 
+// Reset star
+function resetStar(star) {
+  star.classList.add("fa-star");
+  star.classList.remove("fa-star-o");
+}
+
+// Remove star
+function removeStar(star) {
+  star.classList.remove("fa-star");
+  star.classList.add("fa-star-o");
+  starRating--;
+}
+
+// Check if open cards match
 function checkForMatch() {
-  // if the cards do match, lock the cards in the open position and increment the matches count
+  // If the cards do match, lock the cards in the open position and increment the match counter
   if (openCards[0].querySelector("i").classList[1] ===
     openCards[1].querySelector("i").classList[1]) {
     for (card of openCards) {
       card.classList.add("match");
-      matches++;
     }
+    matchCount++;
   }
-  // if the cards do not match, hide the card's symbol
+  // If the cards don't match, hide the card's symbol
   else {
     for (card of openCards) {
       toggleCard(card);
     }
   }
 
-  // remove the cards from the list of open cards
+  // Clear the list of open cards
   openCards = [];
 
-  // increment the move counter and display it on the page
-  moves++;
-  document.querySelector(".moves").textContent = moves;
+  // Increment the move counter and display it on the page
+  moveCount++;
+  moves.textContent = moveCount;
 
-  // TODO: if all cards have matched, display a message with the final score
-  if (matches === 16) {
-    console.log("game over");
+  // After 8 moves, star rating decreases to 2
+  if (moveCount === 9) {
+    removeStar(stars.children[2].querySelector("i"));
+  }
+  // After 16 moves, star rating decreases to 1
+  else if (moveCount === 17) {
+    removeStar(stars.children[1].querySelector("i"));
+  }
+
+  // If all cards have matched, display modal with final score
+  if (matchCount === 16) {
+    const totalTime = performance.now() - startTime;
+    document.querySelector(".stat-time").textContent = totalTime;
+    document.querySelector(".stat-moves").textContent = moveCount;
+    document.querySelector(".stat-stars").textContent = starRating;
+    winModal.show();
   }
 }
 
-let openCards = [], matchedCards = [], moves = 0, matches = 0;
-const deck = document.querySelector(".deck");
+// Set card event listener
 deck.addEventListener("click", function(e) {
   if (e.target.classList.contains("card")) {
-     const card = e.target;
+    // Get selected card
+    const card = e.target;
 
-     // Display the card's symbol
-     toggleCard(card);
+    // Display the card's symbol
+    toggleCard(card);
 
-     // Add the card to a *list* of "open" cards
-     openCards.push(card);
+    // Add the card to a list of open cards
+    openCards.push(card);
 
-     // If the list already has another card, check to see if the two cards match
-     if (openCards.length === 2) {
-       setTimeout(function() {
-         checkForMatch();
-       }, 600);
-     }
- }
+    // If the list already has another card, check to see if the two cards match
+    if (openCards.length === 2) {
+      setTimeout(function() {
+        checkForMatch();
+      }, 600);
+    }
+  }
 });
