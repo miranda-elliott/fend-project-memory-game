1c9a5d954a594323890399e4fc8fc5b14b8f467b
Miranda Elliott <melliott@v4development.com>
2018-02-04 22:46:31 -0500

    Set up event listener for card

--- js/app.js
+++ js/app.js
@@ -36,3 +36,57 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+function toggleCard(card) {
+  card.classList.toggle("open");
+  card.classList.toggle("show");
+}
+
+function checkForMatch() {
+  // if the cards do match, lock the cards in the open position and increment the matches count
+  if (openCards[0].querySelector("i").classList[1] ===
+    openCards[1].querySelector("i").classList[1]) {
+    for (card of openCards) {
+      card.classList.add("match");
+      matches++;
+    }
+  }
+  // if the cards do not match, hide the card's symbol
+  else {
+    for (card of openCards) {
+      toggleCard(card);
+    }
+  }
+
+  // remove the cards from the list of open cards
+  openCards = [];
+
+  // increment the move counter and display it on the page
+  moves++;
+  document.querySelector(".moves").textContent = moves;
+
+  // TODO: if all cards have matched, display a message with the final score
+  if (matches === 16) {
+    console.log("game over");
+  }
+}
+
+let openCards = [], matchedCards = [], moves = 0, matches = 0;
+const deck = document.querySelector(".deck");
+deck.addEventListener("click", function(e) {
+  if (e.target.classList.contains("card")) {
+     const card = e.target;
+
+     // Display the card's symbol
+     toggleCard(card);
+
+     // Add the card to a *list* of "open" cards
+     openCards.push(card);
+
+     // If the list already has another card, check to see if the two cards match
+     if (openCards.length === 2) {
+       setTimeout(function() {
+         checkForMatch();
+       }, 600);
+     }
+ }
+});
